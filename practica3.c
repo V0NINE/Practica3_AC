342d2b45bb5eace366767b9b44cff5d5367c7f91
Victor Fosch <victor.fosch@estudiants.urv.cat>
2024-01-04 12:34:48 +0100

    victor: qs amb threads (no provat)

--- practica3.c
+++ practica3.c
@@ -1,41 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#define NN 512000000
+#include <pthread.h>
+
+#define NN 512000000  
 #define MAX_INT ((int)((unsigned int)(-1)>>1))
 
 int valors[NN+1];
 int valors2[NN+1];
 
-void qs(int *val, int ne)
+typedef struct {
+    int *val;
+    int ne;
+}qs_args;
+
+void *qs(void *arg)
 {
+    qs_args *args = (qs_args *)arg;
+
     int i,f,j;
     int pivot,vtmp,vfi;
-    pivot = val[0];
+    pivot = args->val[0];
     i = 1;
-    f = ne-1;
-    vtmp = val[i];
+    f = args->ne-1;
+    vtmp = args->val[i];
 
     while (i <= f)
     {
         if (vtmp < pivot) 
         {
-            val[i-1] = vtmp;
+            args->val[i-1] = vtmp;
             i ++;
-            vtmp = val[i];
+            vtmp = args->val[i];
         }
         else {
-            vfi = val[f];
-            val[f] = vtmp;
+            vfi = args->val[f];
+            args->val[f] = vtmp;
             f --;
             vtmp = vfi;
         }
     }
 
-    val[i-1] = pivot;
+    args->val[i-1] = pivot;
 
-    if (f>1) qs(val,f);
-    if (i < ne-1) qs(&val[i],ne-f-1);
+    if (f > 1)      //if (f>1) qs(val,f);
+    {
+        qs_args args_qs;
+        args_qs.val = args->val;
+        args_qs.ne = f;
+        qs(&args_qs);
+    }
+    if(i < args->ne-1)     //if (i < ne-1) qs(&val[i],ne-f-1);
+    {
+        qs_args args_qs;
+        args_qs.val = &args->val[i];
+        args_qs.ne = args->ne-f-1;
+        qs(&args_qs);
+    }
 }
 
 void merge2(int* val, int n,int *vo)
@@ -65,20 +86,31 @@ int main(int nargs,char* args[])
 
     parts = atoi(args[2]);
 
+    pthread_t threads[parts];
+    qs_args args_qs[parts];
+    
     if (parts < 2) assert("Han d'haver dues parts com a minim" == 0);
     if (ndades % parts) assert("N ha de ser divisible per parts" == 0);
 
     for(i=0;i<ndades;i++) valors[i]=rand()%MAX_INT;
 
+    for(i = 0; i < parts; i++)
+    {
+        args_qs[i].val = &valors[i*(ndades/parts)];     // primera direcció de memòria del vector per cada thread
+        args_qs[i].ne = ndades/parts;                  // nombre d'elements que ha de tractar cada thread
+
+        pthread_create(&threads[i], NULL, qs, (void *)&args[i]);
+    }
+
     // quicksort original, per fer PROVES
     // qs(valors,ndades);
     // vin=valors;
     // Quicksort a parts, Cada part l'hauria de fer un thread !!
-    for (i=0;i<parts;i++)
+   /* for (i=0;i<parts;i++)
     {
         // printf("de %d a %d\n",i*ndades/parts,(i+1)*(ndades/parts));
         qs(&valors[i*(ndades/parts)],ndades/parts);
-    }
+    }*/
 
     // Merge de dos vectors. 1 de cada 2 threads uneix els dos vectors
     // A cada volta nomes treballen la meitat dels threads fins arribar
@@ -109,7 +141,7 @@ int main(int nargs,char* args[])
     merge2(valors2,N,valors); //per a TH0
     vin=valors;
     */
-    for (i=1;i<ndades;i++) assert(vin[i-1]<=vin[i]);
+    for (i=1;i<ndades;i++) assert(vin[i-1]<=vin[i]); // comprova que la llista estigui ordenada
 
     for (i=0;i<ndades;i+=100)
         sum += vin[i];
