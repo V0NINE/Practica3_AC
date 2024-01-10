3365a2fa021e773f4869476b3703f0736f3fadc9
Miguel Robledo <mrobledo027@gmail.com>
2024-01-10 18:04:05 +0100

    no funciona a molts casos

--- practica3.c
+++ practica3.c
@@ -26,7 +26,7 @@ void *qs(void *arg)
     f = args->ne-1;
     vtmp = args->val[i];
 
-    printf("Hola qs, visita del thread %d, el meu primer element es: %d\n", args->pid, args->val[0]);
+    //printf("Hola qs, visita del thread %d, el meu primer element es: %d\n", args->pid, args->val[0]);
 
     while (i <= f)
     {
@@ -78,7 +78,7 @@ void merge2(int* val, int n,int *vo)
 
 int main(int nargs,char* args[])
 {
-    int ndades,i,m,parts;
+    int ndades,i,m,parts, t;
     int *vin,*vout,*vtmp;
     long long sum=0;
 
@@ -97,62 +97,58 @@ int main(int nargs,char* args[])
 
     for(i=0;i<ndades;i++) valors[i]=rand()%MAX_INT;
 	
-    for(i=0;i<ndades;i++)
-	printf("%d\n", valors[i]);
+    // printf("-----VALORS ORIGINALS-----\n");
+    // for(i=0;i<ndades;i++)
+	// printf("%d\n", valors[i]);
 
     for(i = 0; i < parts; i++)
     {
         args_qs[i].val = &valors[i*(ndades/parts)];     // primera direcció de memòria del vector per cada thread
         args_qs[i].ne = ndades/parts;			// nombre d'elements que ha de tractar cada thread
-	args_qs[i].pid = i;
-	printf("thread %d primer valor: %d\n", args_qs[i].pid, args_qs[i].val[0]);
-
+	    args_qs[i].pid = i;
+	    //printf("thread %d primer valor: %d\n", args_qs[i].pid, args_qs[i].val[0]);
         pthread_create(&threads[i], NULL, qs, (void *)&args_qs[i]);
     }
 
-    // quicksort original, per fer PROVES
-    // qs(valors,ndades);
-    // vin=valors;
-    // Quicksort a parts, Cada part l'hauria de fer un thread !!
-   /* for (i=0;i<parts;i++)
+    
+    // Sincronització i merge dos a dos
+    for (int step = 1; step < parts; step *= 2) 
     {
-        // printf("de %d a %d\n",i*ndades/parts,(i+1)*(ndades/parts));
-        qs(&valors[i*(ndades/parts)],ndades/parts);
-    }*/
 
-    // Merge de dos vectors. 1 de cada 2 threads uneix els dos vectors
-    // A cada volta nomes treballen la meitat dels threads fins arribar
-    // al thread principal
-    /*vin = valors;
-    vout = valors2;
-    for (m = 2*(ndades/parts); m <= ndades; m *= 2)
-    {
-        for (i = 0; i < ndades; i += m)
-            merge2(&vin[i],m,&vout[i]);
+        for (i = 0; i < parts; i += 2 * step) 
+        {
+            if (i + step < parts) 
+            {
+                // Fusiona les dos parts: [i, i+step)
+                merge2(&valors[i * ndades/parts], 2 * ndades/parts, &valors2[i * ndades/parts]);
+            }
+        }
 
-        vtmp=vin;
-        vin=vout;
-        vout=vtmp;
-    }*/
+        // Intercanvia vin y vout per a la següent ronda de merge
+        vtmp = vin;
+        vin = vout;
+        vout = vtmp;
 
-    /* EXEMPLE ASSIGNACIO FEINA a 4 THREADS
-    qs(&valors[0],ndades/4); //per a TH0
-    qs(&valors[ndades/4],ndades/4); //per a TH1
-    qs(&valors[ndades/2],ndades/4); //per a TH2
-    qs(&valors[3*ndades/4],ndades/4); //per a TH3
-    // sincro barrera
-    merge2(&valors[0],ndades/2,&valors2[0]); //per a TH0
-    merge2(&valors[ndades/2],ndades/2,&valors2[ndades/2]); //per a TH2
-
-    /sincro barrera
-
-    merge2(valors2,N,valors); //per a TH0
+        // Redueix la quantitat de threads actius a la meitat
+        for (int j = 0; j < parts; j += 2 * step) 
+        {
+            if (j + step < parts) 
+            {
+                pthread_join(threads[j + step], NULL);
+            }
+        }
+    }
+    merge2(valors2,ndades,valors); //per a TH0
     vin=valors;
-    */
-    /*for (i=1;i<ndades;i++) assert(vin[i-1]<=vin[i]); // comprova que la llista estigui ordenada
+    
+    // printf("-----VALORS ORDENATS-----\n");
+    // for(i=0;i<ndades;i++)
+	// printf("%d\n", valors[i]);
+
+    for (i=1;i<ndades;i++) assert(vin[i-1]<=vin[i]); // comprova que la llista estigui ordenada
 
     for (i=0;i<ndades;i+=100)
         sum += vin[i];
     
-    printf("validacio %lld \n",sum);*/
+    printf("validacio %lld \n",sum);
 }
